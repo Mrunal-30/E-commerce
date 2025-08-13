52aba81c7fa58fed5f75981b382b392b003cbc3d
Mrunal-30 <mrunalchalakh30@gmail.com>
2025-08-13 11:45:54 +0530

    Commit-5

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  optimizeDeps: {
-    include: ['react-router-dom', 'react-toastify']
-  }
+  plugins: [react(), tailwindcss()],
+  server: { port: 5174 },
 });
